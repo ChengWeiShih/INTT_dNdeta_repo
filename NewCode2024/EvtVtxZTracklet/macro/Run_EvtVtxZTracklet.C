31a2d23f0a6c0d52fa2bbab519a5c5caf9dfc3fc
Cheng Wei <ecie9969@sphnx04.sdcc.bnl.gov>
2024-12-12 00:04:30 -0500

    Upload the folder for the new code for the sPHENIX dNdeta analysis

---
+++ NewCode2024/EvtVtxZTracklet/macro/Run_EvtVtxZTracklet.C
@@ -0,0 +1,77 @@
+#include "../EvtVtxZProtoTracklet.h"
+
+R__LOAD_LIBRARY(../libEvtVtxZProtoTracklet.so)
+
+void Run_EvtVtxZTracklet(
+  int process_id = 0,
+  int run_num = 54280,
+  int nevents = -1,
+  string input_directory = "/sphenix/user/ChengWei/INTT/INTT/general_codes/CWShih/INTTBcoResolution/macro",
+  string input_filename = "file_list_54280_intt.txt",
+  string output_directory = "/sphenix/tg/tg01/commissioning/INTT/work/cwshih/seflgendata/run_54280/completed/BCO_check",
+  
+  // todo : modify here
+  std::string output_file_name_suffix = "_test20241210",
+
+  std::pair<double, double> vertexXYIncm = {-0.02204, 0.2229}, // note : in cm
+  bool IsFieldOn = false,
+  bool IsDCACutApplied = 1,
+  std::pair<std::pair<double,double>,std::pair<double,double>> DeltaPhiCutInDegree = {{-0.5, 0.5},{-1000.,1000.}}, // note : in degree
+  std::pair<std::pair<double,double>,std::pair<double,double>> DCAcutIncm = {{-0.1, 0.1},{-1000.,1000.}}, // note : in cm
+  int ClusAdcCut = 35,
+  int ClusPhiSizeCut = 5,
+  
+  bool PrintRecoDetails = 1,
+  bool DrawEvtVtxZ = 1,
+
+  bool RunInttBcoFullDiff = 1,
+  bool RunVtxZReco = 1,
+  bool RunTrackletPair = 1,
+  bool RunTrackletPairRotate = 1,
+  
+  bool HaveGeoOffsetTag = false
+)
+{
+
+  EvtVtxZProtoTracklet * evzpt = new EvtVtxZProtoTracklet(
+    process_id,
+    run_num,
+    nevents,
+    input_directory,
+    input_filename,
+    output_directory,
+    output_file_name_suffix,
+
+    vertexXYIncm,
+    IsFieldOn,
+    IsDCACutApplied,
+    DeltaPhiCutInDegree,
+    DCAcutIncm,
+    ClusAdcCut,
+    ClusPhiSizeCut,
+      
+    PrintRecoDetails,
+    DrawEvtVtxZ,
+
+    RunInttBcoFullDiff,
+    RunVtxZReco,
+    RunTrackletPair,
+    RunTrackletPairRotate,
+      
+    HaveGeoOffsetTag
+  );
+
+  string final_output_file_name = evzpt->GetOutputFileName();
+  string plot_final_output_file_name = "Plot_"+final_output_file_name;
+  cout<<"final_output_file_name: "<<final_output_file_name<<endl;
+  system(Form("if [ -f %s/completed/%s ]; then rm %s/completed/%s; fi;", output_directory.c_str(), final_output_file_name.c_str(), output_directory.c_str(), final_output_file_name.c_str()));  
+  system(Form("if [ -f %s/completed/%s ]; then rm %s/completed/%s; fi;", output_directory.c_str(), plot_final_output_file_name.c_str(), output_directory.c_str(), plot_final_output_file_name.c_str()));  
+
+  evzpt -> MainProcess();
+  evzpt -> EndRun();
+
+  system(Form("mv %s/%s %s/completed", output_directory.c_str(), final_output_file_name.c_str(), output_directory.c_str()));
+  system(Form("mv %s/%s %s/completed", output_directory.c_str(), plot_final_output_file_name.c_str(), output_directory.c_str()));
+
+  return;
+}
---
+++ NewCode2024/TrackletHistogram/TrackletHistogramFill.h
@@ -0,0 +1,170 @@
+#ifndef TRACKLETHISTOGRAMFILL_H
+#define TRACKLETHISTOGRAMFILL_H
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include <numeric>
+
+#include <TFile.h>
+#include <TTree.h>
+#include <TH2D.h>
+#include <TH1D.h>
+#include <TMath.h>
+
+#include <TObjArray.h>
+
+#include "../EvtVtxZTracklet/structure.h"
+
+class TrackletHistogramFill{
+    
+    public:
+        TrackletHistogramFill(
+            int process_id_in,
+            int runnumber_in,
+
+            std::string output_directory_in,
+            std::string output_file_name_suffix_in
+        );
+
+        void SetvtxZReweight(bool tag){ vtxZReweight = tag; }
+        void SetBcoFullDiffCut(bool tag){ BcoFullDiffCut = tag; }
+        void SetINTT_vtxZ_QA(bool tag){ INTT_vtxZ_QA = tag; }
+        void SetWithRotate(bool tag){ isWithRotate = tag; }
+
+        void SetVtxZReweightHist(TH1D * input_hist) {h1D_vtxZReweightFactor = (TH1D*)input_hist->Clone();}
+
+        void PrepareOutPutFileName();
+        std::string GetOutputFileName() {return output_filename;}
+        void PrepareOutPutRootFile();
+        void PrepareHistograms();
+        void FillHistogram(
+            // note : MBD & centrality relevant
+            float MBD_z_vtx,
+            int is_min_bias,
+            float MBD_centrality,
+            float MBD_south_charge_sum,
+            float MBD_north_charge_sum,
+            // float MBD_charge_sum,
+            // float MBD_charge_asymm,
+            int InttBcoFullDiff_next,
+
+            // // note : trigger tag
+            // int MBDNSg2,
+            // int MBDNSg2_vtxZ10cm,
+            // int MBDNSg2_vtxZ30cm,
+            // int MBDNSg2_vtxZ60cm,
+
+            // std::vector<float> *ClusX,
+            // std::vector<float> *ClusY,
+            // std::vector<float> *ClusZ,
+            // std::vector<int> *ClusLayer,
+            std::vector<unsigned char> *ClusLadderZId,
+            // std::vector<unsigned char> *ClusLadderPhiId,
+            std::vector<int> *ClusAdc,
+            std::vector<float> *ClusPhiSize,
+
+            // note : INTT vertex Z
+            double INTTvtxZ,
+            double INTTvtxZError,
+            // double NgroupTrapezoidal,
+            // double NgroupCoarse,
+            double TrapezoidalFitWidth,
+            double TrapezoidalFWHM,
+
+            // note : the tracklet pair
+            std::vector<pair_str> *evt_TrackletPair_vec,
+            std::vector<pair_str> *evt_TrackletPairRotate_vec,
+
+            // note : MC
+            // float TruthPV_trig_x,
+            // float TruthPV_trig_y,
+            float TruthPV_trig_z,
+            int NTruthVtx,
+            int NPrimaryG4P,
+            // std::vector<float> *PrimaryG4P_Pt,
+            std::vector<float> *PrimaryG4P_Eta,
+            // std::vector<float> *PrimaryG4P_Phi,
+            // std::vector<float> *PrimaryG4P_E,
+            // std::vector<float> *PrimaryG4P_PID,
+            std::vector<int> *PrimaryG4P_isChargeHadron
+        );
+        void EndRun();
+
+    protected:
+
+        // note : ----------------- for the constructor -----------------
+        int process_id;
+        int runnumber;
+        std::string output_directory;
+        std::string output_file_name_suffix;
+
+        bool vtxZReweight;
+        bool BcoFullDiffCut;
+        bool INTT_vtxZ_QA;
+        bool isWithRotate;
+
+        // note : ----------------- for the root file out -----------------
+        TFile * file_out;
+        TTree * tree_out_par;
+        std::string output_filename;
+
+        // note : ----------------- for the loop -----------------
+        void EvtCleanUp();
+        std::map<int,int> Used_Clus_index_map;
+        std::vector<double> Pair_DeltaR_vec;
+
+
+        // note : ----------------- for the histogram -----------------
+        std::map<std::string,TH1D*> h1D_map;
+        std::map<std::string, TH2D*> h2D_map;
+
+        // note : ----------------- for the centrality -----------------
+        TH1D * h1D_centrality_bin;
+        // int convert_centrality_bin (double centrality_in);
+        
+
+        // note : ----------------- for the reweights -----------------
+        TH1D * h1D_vtxZReweightFactor = nullptr;
+        double vtxZReweightFactor;
+
+        // note : ----------------- constants -----------------
+        std::vector<double> centrality_edges = {0, 0.05, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0};
+        int nCentrality_bin;
+
+        // note : for centrality fine
+        double CentralityFineEdge_min = -0.005;
+        double CentralityFineEdge_max = 1.005;
+        int nCentralityFineBin = 101;
+        
+        // note : for eta (tracklet)
+        double EtaEdge_min = -2.7;
+        double EtaEdge_max = 2.7;
+        int nEtaBin = 27;
+        TH1D * h1D_eta_template;
+
+        // note : for z
+        double VtxZEdge_min = -45; // note : cm
+        double VtxZEdge_max = 45; // note : cm
+        int nVtxZBin = 18;
+        TH1D * h1D_vtxz_template;
+
+        // note : for deta_phi
+        double DeltaPhiEdge_min = -0.05; // note : rad ~ -4 degree
+        double DeltaPhiEdge_max = 0.05;  // note : rad ~ 4 degree
+        int    nDeltaPhiBin = 100;
+
+        std::vector<int> typeA_sensorZID = {0,2}; // note : sensor Z ID for type A // note -> 1, 0, 2, 3        
+
+        std::pair<double, double> cut_MBDvtxZ    = {-10., 10.};
+        std::pair<double, double> cut_vtxZDiff = {-2, 3.};
+        std::pair<double, double> cut_TrapezoidalFitWidth = {1.5, 5.5};
+        std::pair<double, double> cut_TrapezoidalFWHM = {2,8};
+
+        int cut_InttBcoFullDIff_next = 188;
+
+        // note : for the best pair
+        std::pair<double, double> cut_bestPair_DeltaR = {0, 0.05}; // note : rad
+};
+
+#endif // TRACKLETHISTOGRAMFILL_H
